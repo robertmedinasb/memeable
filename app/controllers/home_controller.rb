af68c4aac9905b33d1c5d2f1ed38191678f7932c
Victor Rodriguez <victor.rodriguez.guriz@gmail.com>
2020-05-29 22:35:01 -0500

    Added Robert's part I

--- app/controllers/home_controller.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class HomeController < ApplicationController
-  before_action :authenticate_user!, only: :show
-
-  def index
-    @memes = Meme.all
-  end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   get 'vote/:id', to: 'memes#vote'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'home#index'
+  root to: 'memes#index'
 end
